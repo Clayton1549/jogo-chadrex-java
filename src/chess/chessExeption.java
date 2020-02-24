b54cf5bfea48c99e168b329009ceb477fcb0cdd4
Clayton1549 <pereira.cleiton.oliver@gmail.com>
2020-02-24 13:17:54 -0300

    ChessExeption and ChessPosition

---
+++ src/chess/chessExeption.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class chessExeption extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public  chessExeption(String msg) {
+    	
+    	super(msg);
+    	
+    }
+}
